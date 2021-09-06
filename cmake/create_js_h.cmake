18b51329e85b964d113da37bbf051b34f63149b9
Matthew Ratzloff <matthew@ratzloff.name>
2021-09-06 17:04:22 -0400

    Reorganize

--- cmake/create_js_h.cmake
+++ cmake/create_js_h.cmake
@@ -1,6 +1,6 @@
 function(create_js_h)
-	file(READ CMakeFiles/CmakeTmp/uhs.js data)
-	file(WRITE include/js.h "R\"\"\"(" ${data} ")\"\"\";")
+	file(READ ts/build/uhs.js data)
+	file(WRITE cpp/include/js.h "R\"\"\"(" ${data} ")\"\"\";")
 endfunction()
 
 create_js_h()
