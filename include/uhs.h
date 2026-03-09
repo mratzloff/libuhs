54f224dc8c5aa5f14ce6a4493f58a349f7072fbb
Matthew Ratzloff <matthew@ratzloff.name>
2026-03-08 19:37:55 -0700

    Fix bug with smaller table header separator

--- include/uhs.h
+++ include/uhs.h
@@ -362,7 +362,7 @@ namespace Regex {
 std::regex const Descriptor{"([0-9]+) ([a-z]{4,})"};
 std::regex const DataAddress{"0{6} ?([0-3])? ([0-9]{6,}) ([0-9]{6,})"};
 std::regex const EmailAddress{".+@.+\\..+"};
-std::regex const HorizontalLine{"(?:-{4,} *)+"};
+std::regex const HorizontalLine{"(?:-{3,} *)+"};
 std::regex const HyperpngRegion{
     "(-?[0-9]{3,}) (-?[0-9]{3,}) (-?[0-9]{3,}) (-?[0-9]{3,})"};
 std::regex const OverlayAddress{
