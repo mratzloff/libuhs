aea2e6a9377eb743b7836e083550d9008355ab56
Matthew Ratzloff <matthew@ratzloff.name>
2026-03-28 09:50:52 -0700

    Fix initializer list ordering

--- cpp/src/writer.cpp
+++ cpp/src/writer.cpp
@@ -3,7 +3,7 @@
 namespace UHS {
 
 Writer::Writer(Logger const& logger, std::ostream& out, Options const& options)
-    : logger_{logger}, out_{out}, options_{options} {}
+    : logger_{logger}, options_{options}, out_{out} {}
 
 void Writer::reset() {}
 
